6492586f5f317b4c4bc4ca035b5d5ea578b8ec26
ZionDeng <569598401@qq.com>
2020-11-03 21:20:39 +0800

    learning merge operation

--- 1.py
+++ 1.py
@@ -2,3 +2,7 @@ a =1
 # add another comment
 # use vs code to git 
 # changed in dev branch 
+# %%
+# git checkout dev -> create a dev branch and switch to it 
+# git checkout master -> back to master branch 
+# # $ git merge --no-ff -m "keep merge info" dev: merge 2 branches into master, with log info 
