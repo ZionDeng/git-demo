729de47c65eab14f369d94f07f5f6de3e312ad18
ZionDeng <569598401@qq.com>
2020-11-03 21:40:53 +0800

    conflict branches1

--- 1.py
+++ 1.py
@@ -2,7 +2,15 @@ a =1
 # add another comment
 # use vs code to git 
 # changed in dev branch 
-# %%
+# %% branch 
+
+# git checkout -b branch_name -> build a branch and switch to it
+
+# %% merge 
 # git checkout dev -> create a dev branch and switch to it 
 # git checkout master -> back to master branch 
 # # $ git merge --no-ff -m "keep merge info" dev: merge 2 branches into master, with log info 
+
+# %% conflict situation 
+
+# we have 2 branch now, panda and dev
